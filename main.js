929ec45b90e5ab148287a0e6836e3abac3dcb18c
LauraDem <laura93dem@gmail.com>
2023-12-22 03:21:13 +0100

    add filter to contacts search

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var dateTime = luxon.DateTime;
+let dateTime = luxon.DateTime;
 
 const { createApp } = Vue;
 
@@ -8,57 +8,59 @@ createApp({
       contacts,
       activeContactIndex: 0,
 
-      newMessage:{
-        date:'',
-        message: '',
-        status: 'sent',
-      }, 
+      newMessage: {
+        date: dateTime.local().toFormat("dd/MM/yyyy h:mm:ss"),
+        message: "",
+        status: "sent",
+      },
+      searchTerm: "",
     };
   },
 
-
-
   methods: {
-    // CONTATTO ATTIVO 
-    setActiveContactIndex (index) {
+    // Creazione del metodo filteredContacts: Abbiamo creato un nuovo metodo chiamato filteredContacts, che restituisce una lista di contatti filtrata in base al termine di ricerca. Questo metodo utilizza il metodo toLowerCase() per rendere la ricerca case-insensitive e includes per verificare se il nome del contatto contiene il termine di ricerca.
+    filteredContacts() {
+      const searchTerm = this.searchTerm.toLowerCase();
+      return this.contacts.filter((contact) =>
+        contact.name.toLowerCase().includes(searchTerm)
+      );
+    },
+    // CONTATTO ATTIVO
+    setActiveContactIndex(index) {
       this.activeContactIndex = index;
     },
 
-    // NUOVO MESSAGGIO 
+    // NUOVO MESSAGGIO
     sendNewMessage() {
-      if (this.newMessage.message == '') return;
+      if (this.newMessage.message == "") return;
 
       const newMessageStatic = { ...this.newMessage };
       this.contacts[this.activeContactIndex].messages.push(newMessageStatic);
-      this.newMessage.message = '';
+      this.newMessage.message = "";
 
-
-      // RISPOSTA AUTOMATICA 
+      // RISPOSTA AUTOMATICA
       setTimeout(() => {
         const newAnswer = {
-          date: '...',
-          message: 'Ok',
-          status: 'received',
-        }
+          date: dateTime.local().toFormat("dd/MM/yyyy h:mm:ss"),
+          message: "Ok",
+          status: "received",
+        };
 
         this.contacts[this.activeContactIndex].messages.push(newAnswer);
-
-      }, 1000)
+      }, 1000);
     },
 
     timeToText(timestring) {
-
-
-      const messageDateTime = dateTime.fromFormat(timestring, 'dd/MM/yyyy h:mm:ss');
+      const messageDateTime = dateTime.fromFormat(
+        timestring,
+        "dd/MM/yyyy h:mm:ss"
+      );
 
       const timeText = messageDateTime.toLocaleString(dateTime.TIME_SIMPLE);
 
-      return(timeText);
+      return timeText;
     },
   },
 
-  mounted() {
-    
-  },
-
-}).mount('#app');
+  mounted() {},
+}).mount("#app");
